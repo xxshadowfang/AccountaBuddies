6a4ed5b1853608ab4002cf463cba74bb3bfd89e1
Tianjiao Mo <mot@rose-hulman.edu>
2015-10-21 20:09:24 -0400

    123

---
+++ Mo scripts/createUser.sql
@@ -0,0 +1,53 @@
+-- USE AccountaBuddiesDatabase;
+-- GO
+-- CREATE PROCEDURE createUser 
+--     @Username nvarchar(50) NOT NULL,
+--     @salt nvarchar(50) NOT NULL, 
+--     @FirstName nvarchar(50),
+--     @LastName nvarchar(50),
+--     @Age int,
+--     @Gender char,
+--     @cookie nvarchar(50) NOT NULL
+-- AS 
+-- BEGIN TRY
+--     SET NOCOUNT ON;
+--     BEGIN TRANSACTION
+--     	IF (COUNT(Select * FROM usersTable WHERE userName = @Username) == 0)
+-- 		    INSERT INTO usersTable (userName, saltedPassword, FirstName, LastName, age, gender,cookie)
+-- 		    VALUES (@Username, @salt, @FirstName, @LastName, @Age, @Gender,@cookie)
+--             RETURN 0;  
+-- 	    ELSE
+-- 	    	RETURN 1;
+--     END TRANSACTION
+-- END TRY
+-- BEGIN CATCH
+-- 	--error handling/printing
+-- END CATCH
+-- GO
+
+
+CREATE DEFINER=``@`` PROCEDURE `registerUser`(
+    IN _username nvarchar(50), 
+    IN _salt nvarchar(50),
+    IN _FirstName nvarchar(50),
+    IN _LastName nvarchar(50),
+    IN _Age int,
+    IN _Gender char,
+    IN _cookie nvarchar(50)
+)
+
+BEGIN
+    if (username IS NULL) THEN SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'username was null';
+        END IF;
+    if (salt IS NULL) THEN SIGNAL SQLSTATE '45001'
+        SET MESSAGE_TEXT = 'password was null';
+        END IF;
+    if (cookie IS NULL) THEN SIGNAL SQLSTATE '45002'
+        SET MESSAGE_TEXT = 'cookie was null';
+        END IF;
+
+   
+    INSERT INTO user (username, salt,FirstName,LastName,Age,Gender,cookie) 
+    VALUES (_username, _salt,_FirstName,_LastName,_Age,_Gender,_cookie);
+END
--- Mo scripts/userScript.sql
+++ Mo scripts/userScript.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) NOT NULL,
-  `salted_pw` char(60) NOT NULL,
-  `first_name` varchar(15) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `salt` char(60) NOT NULL,
+  `FirstName` varchar(15) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `Age` int,
+  `Gender` char,
   `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
@@ -13,7 +15,7 @@ CREATE TABLE `user` (
 SELECT * FROM user;
 
 -- prepared statement
-INSERT INTO `user` (`username`, `salted_pw`, `first_name`, `last_name`)
+INSERT INTO `user` (`Username`, `salt`, `FirstName`, `LastName`,`Age`,`Gender`)
 VALUES (
 	'Collin', 'trowbrct@rose-hulm3an.edu', '2309487320852', 'cdog', 'trow'
 );
