7c6fa774ad488babfe50cc748f172ded60766314
Tianjiao Mo <mot@rose-hulman.edu>
2015-10-23 21:29:03 -0400

    current

--- AccountaBuddies/tasks/pipeline.js
+++ AccountaBuddies/tasks/pipeline.js
@@ -32,7 +32,7 @@ var jsFilesToInject = [
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js',
+  'js/dependencies2/**/*.js',
 
   // Use the "exclude" operator to ignore files
   // '!js/ignore/these/files/*.js'
