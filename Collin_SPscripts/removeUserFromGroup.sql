99f7286e0cd18d50b3435a1909e2c529d52985e8
Collin Trowbridge <trowbrct@rose-hulman.edu>
2015-10-25 18:52:15 -0400

    Added another group stored procedure

---
+++ Collin_SPscripts/removeUserFromGroup.sql
@@ -0,0 +1,20 @@
+CREATE DEFINER=``@`` PROCEDURE `removeUserFromGroup`(
+	IN _userId int,
+    IN _groupId int
+)
+BEGIN
+	if (_userId IS NULL) THEN SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'userId was null';
+        END IF;
+	if (_groupId IS NULL) THEN SIGNAL SQLSTATE '45001'
+        SET MESSAGE_TEXT = 'groupId was null';
+        END IF;
+        
+	IF (SELECT COUNT(*) FROM group_users__user_groups WHERE user_groups = _userId AND group_users = _groupId) = 0 THEN
+		SIGNAL SQLSTATE '45002'
+        SET MESSAGE_TEXT = 'user is not in this group';
+        END IF;
+        
+	DELETE FROM `group_users__user_groups`
+    WHERE group_users = _groupId AND user_groups = _userId;
+END
