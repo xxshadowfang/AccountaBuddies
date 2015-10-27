7c082ef2ffeaf319fc7f89872e830b9c224094b0
Collin Trowbridge <trowbrct@rose-hulman.edu>
2015-10-27 10:35:59 -0400

    Another bad commit. hopefully wont commit

--- AccountaBuddies/test/REST/UserContoller.test.js
+++ AccountaBuddies/test/REST/UserContoller.test.js
@@ -1,16 +1,23 @@
 var request = require('supertest');
 
 describe('UserController', function() {
-	
+
 	describe('login tests', function() {
 		it('should return an okay status for login', function(done) {
 			console.log("user controller tests");
-			request(sails.hooks.http.app)
-				.post('/user/register')
-				.send({ username: 'collin', password: 'test'})
-				.expect(300, done);
-				// TODO: write real tests
-				//done();
+			request(sails.hooks.http.app).post('/user/register').send({
+				username : 'collin',
+				password : 'test'
+			}).expect(300, done);
+			// TODO: write real tests
+			// done();
 		});
 	});
+});
+
+afterEach(function() {
+	if (this.currentTest.state == 'failed') {
+		console.log("we failed!!")
+		return -1;
+	}
 });
